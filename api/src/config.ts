0f94efd191e57372073e512abe34ed31a71cdc4c
ringeringeraja <joaosan177@gmail.com>
2024-03-16 05:05:51 -0300

    refactor: move config to separate file

---
+++ api/src/config.ts
@@ -0,0 +1,18 @@
+import type { ApiConfig } from 'aeria'
+
+export const config: ApiConfig = {
+  apiUrl: process.env.API_URL,
+  secret: process.env.APPLICATION_SECRET,
+  database: {
+    mongodbUrl: process.env.MONGODB_URL,
+  },
+  defaultUser: {
+    username: process.env.GODMODE_USERNAME,
+    password: process.env.GODMODE_PASSWORD,
+  },
+  storage: {
+    fs: process.env.STORAGE_PATH,
+    tempFs: process.env.STORAGE_TEMP_PATH,
+  },
+}
+
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import { init } from 'aeria'
+import { config } from './config.js'
 import { router } from './routes/index.js'
 import * as collections from './collections/index.js'
 
@@ -8,21 +9,7 @@ export {
 }
 
 export default init({
-  config: {
-    apiUrl: process.env.API_URL,
-    secret: process.env.APPLICATION_SECRET,
-    database: {
-      mongodbUrl: process.env.MONGODB_URL,
-    },
-    defaultUser: {
-      username: process.env.GODMODE_USERNAME,
-      password: process.env.GODMODE_PASSWORD,
-    },
-    storage: {
-      fs: process.env.STORAGE_PATH,
-      tempFs: process.env.STORAGE_TEMP_PATH,
-    },
-  },
+  config,
   callback: async (context) => {
     return router.install(context)
   },
