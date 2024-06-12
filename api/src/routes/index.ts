1235b288b496a625d7bc766540fd1f3215018106
ringeringeraja <joaosan177@gmail.com>
2024-06-12 03:03:50 -0300

    chore: `npm install` -> `npm ci` in Dockerfile

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,10 +1,19 @@
-import { createRouter } from 'aeria'
+import { createRouter, Result } from 'aeria'
 
 export const router = createRouter()
 
-router.GET('/test', () => {
+router.GET('/test', async (context) => {
+  const { error, result } = await context.collections.person.functions.get({
+    filters: {}
+  })
+
+  if( error ) {
+    return Result.error(error)
+  }
+
   return {
     message: 'Hello, world!',
+    result,
   }
 })
 
