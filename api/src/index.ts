bbf21e302f773aaac4503756253b99555d610481
ringeringeraja <joaosan177@gmail.com>
2024-06-12 19:33:36 -0300

    feat: update api boilerplate

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,14 +3,8 @@ import { config } from './config.js'
 import { router } from './routes/index.js'
 export * as collections from './collections.js'
 
-export {
-  router,
-}
-
 export default init({
   config,
-  callback: (context) => {
-    return router.install(context)
-  },
+  router,
 })
 
