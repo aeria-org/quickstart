8a129e8481be15c2ed32b7d11c2ba2596ef89d1e
ringeringeraja <joaosan177@gmail.com>
2025-04-28 23:09:10 -0300

    chore: add dependabot

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,17 +5,8 @@ import { exec as exec_ } from 'node:child_process'
 import { promisify } from 'node:util'
 
 const exec = promisify(exec_)
-const LOCK_FILENAME = 'create-aeria-app.lock'
 
 const main = async () => {
-  if( !fs.existsSync(LOCK_FILENAME) ) {
-    await exec('npm i dualist@latest')
-    await exec('npm i --workspace=api aeria@latest aeria-sdk@latest')
-    await exec('npm i --workspace=web @aeria-ui/i18n-en@latest aeria-app-layout@latest aeria-ui@latest aeria-sdk@latest')
-
-    await fs.promises.writeFile(LOCK_FILENAME, '')
-  }
-
   if( fs.existsSync('.aeria') ) {
     if( fs.existsSync(path.join('api', '.aeria')) ) {
       for( const file of await fs.promises.readdir('.aeria') ) {
