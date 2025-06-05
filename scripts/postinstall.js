4a8994c1884516ca8faed1c7d2b67b26854e04d3
ringeringeraja <joaosan177@gmail.com>
2025-06-05 19:38:57 -0300

    update scripts

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,34 +6,37 @@ import { promisify } from 'node:util'
 
 const exec = promisify(exec_)
 
+const API_PATH = 'api'
+const WEB_PATH = 'web'
+
 const main = async () => {
   if( fs.existsSync('.aeria') ) {
-    if( fs.existsSync(path.join('api', '.aeria')) ) {
+    if( fs.existsSync(path.join(API_PATH, '.aeria')) ) {
       for( const file of await fs.promises.readdir('.aeria') ) {
-        await fs.promises.rename(path.join('.aeria', file), path.join('api', '.aeria', file))
+        await fs.promises.rename(path.join('.aeria', file), path.join(API_PATH, '.aeria', file))
       }
       await fs.promises.rm('.aeria', {
         recursive: true,
       })
     } else {
-      await fs.promises.rename('.aeria', path.join('api', '.aeria'))
+      await fs.promises.rename('.aeria', path.join(API_PATH, '.aeria'))
     }
   }
 
   if( fs.existsSync('.aeria-ui') ) {
-    if( fs.existsSync(path.join('web', '.aeria-ui')) ) {
+    if( fs.existsSync(path.join(WEB_PATH, '.aeria-ui')) ) {
       for( const file of await fs.promises.readdir('.aeria-ui') ) {
-        await fs.promises.rename(path.join('.aeria-ui', file), path.join('web', '.aeria-ui', file))
+        await fs.promises.rename(path.join('.aeria-ui', file), path.join(WEB_PATH, '.aeria-ui', file))
       }
       await fs.promises.rm('.aeria-ui', {
         recursive: true,
       })
     } else {
-      await fs.promises.rename('.aeria-ui', path.join('web', '.aeria-ui'))
+      await fs.promises.rename('.aeria-ui', path.join(WEB_PATH, '.aeria-ui'))
     }
   }
 
-  for( const cwd of ['api', 'web'] ) {
+  for( const cwd of [API_PATH, WEB_PATH] ) {
     await exec('npm i --package-lock-only --workspaces false', {
       cwd,
     })
