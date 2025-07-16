97dcdd66014dc377deffe4b7936949b201f8276c
ringeringeraja <joaosan177@gmail.com>
2025-07-16 15:31:14 -0300

    refactor: update boilerplate

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,22 +1,4 @@
-import { createRouter, Result } from 'aeria'
-import { contracts } from '../../.aeria/out/index.js'
+import { createRouter } from 'aeria'
 
 export const router = createRouter()
-
-router.GET('/user/getBySlug', async (context) => {
-  const { error, result: user } = await context.collections.user.functions.get({
-    filters: {
-      email: context.request.query.slug,
-    },
-  })
-
-  if( error ) {
-    return context.error(404, {
-      code: 'RESOURCE_NOT_FOUND',
-    })
-  }
-
-  return Result.result(user)
-
-}, contracts.GetUserBySlug)
 
