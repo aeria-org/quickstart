c5ef9f4697293d32aaf2b7a53ceadefa995d1152
ringeringeraja <joaosan177@gmail.com>
2025-07-21 18:22:24 -0300

    refactor: update boilerplate

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,11 @@
-import { useApp, defineOptions } from 'aeria-ui'
+import { useApp } from 'aeria-ui'
 import aeriaEn from '@aeria-ui/i18n-en'
 import { en } from './i18n/index.js'
 import { routes } from './routes.js'
 import Main from './main.vue'
 import NoResults from './components/no-results.vue'
 
-const options = defineOptions({
+const { app, mount } = await useApp({
   component: Main,
   routes,
   i18n: {
@@ -17,10 +17,9 @@ const options = defineOptions({
       ],
     },
   },
+
 })
 
-useApp(options).then(({ app, mount }) => {
-  app.provide('noResultsComponent', NoResults)
-  mount()
-})
+app.provide('noResultsComponent', NoResults)
+mount()
 
