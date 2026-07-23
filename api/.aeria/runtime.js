f5ca7cbbb8c6a18cff1d7e9e2be92c95f172304a
ringeringeraja <joaosan177@gmail.com>
2026-07-23 18:32:03 -0300

    chore: bump to TS 7

--- api/.aeria/runtime.js
+++ api/.aeria/runtime.js
@@ -1,8 +1,8 @@
 import { createInstance } from './instance.js'
 import { getStorage } from './storage.js'
 import { uploader } from './upload.js'
-export const instanceConfig = {"publicUrl":{"development":"http://localhost:3000/api","production":"https://yourdomainhere.local/api"},"storage":{"strategy":"localStorage","namespace":"aeria"},"mirrorPaths":[".aeria","../web/.aeria-ui"]}
-export const url = 'https://yourdomainhere.local/api'
+export const instanceConfig = {"environment":"development","publicUrl":{"development":"http://localhost:3000/api","production":"https://yourdomainhere.local/api"},"storage":{"strategy":"localStorage","namespace":"aeria"},"mirrorPaths":[".aeria","../web/.aeria-ui"]}
+export const url = 'http://localhost:3000/api'
 export const aeria = createInstance(instanceConfig)
 export const storage = getStorage(instanceConfig)
 export const upload = uploader(instanceConfig)
